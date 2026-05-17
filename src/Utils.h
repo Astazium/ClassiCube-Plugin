0bdf61fb4c3ea412e30add440fedc625c405f86f
Astazium <113135541+Astazium@users.noreply.github.com>
2026-05-17 15:03:43 +0300

    Update Utils.h

--- src/Utils.h
+++ src/Utils.h
@@ -1,7 +1,6 @@
 #ifndef UTILS_H
 #define UTILS_H
 
-void* Mem_Set(void* dst, cc_uint8 value, unsigned numBytes);
 void  Chat_AddRaw(const char* raw);
 void  Time_FormatSeconds(cc_string* str, float totalSeconds);
 
